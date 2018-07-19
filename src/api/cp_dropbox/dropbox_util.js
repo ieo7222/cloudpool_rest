c46e4020b9c26aa48a9d3acc918bd98daeba1bd4
ikhwan <whdlrghks0314@gmail.com>
2018-07-20 00:32:19 +0900

    fix the error about select

--- src/api/cp_dropbox/dropbox_util.js
+++ src/api/cp_dropbox/dropbox_util.js
@@ -21,7 +21,7 @@ var mimelist = {
   "document": ['application/x-abiword', 'text/css', 'text/csv', 'application/msword', 'application/vnd.oasis.opendocument.presentation', 'application/vnd.oasis.opendocument.spreadsheet', 'application/vnd.oasis.opendocument.text', 'application/vnd.ms-powerpoint', 'application/vnd.ms-excel'],
   "pdf": ['application/pdf'],
   "video": ['video/x-msvideo', 'video/ogg', 'video/webm', 'video/mp4', 'video/mpeg', 'video/avi', 'video/wmv', 'video/mkv', 'video/mpg'],
-  "image": ['image/gif', 'image/x-icon', 'image/jpeg', 'image/svg+xml', 'image/tiff', 'image/webp', 'image/jpg'],
+  "image": ['image/gif', 'image/x-icon', 'image/jpeg', 'image/svg+xml', 'image/tiff', 'image/webp', 'image/jpg', 'image/png'],
   "audio": ['audio/aac', 'audio/midi', 'audio/ogg', 'audio/x-wav', 'audio/webm', 'audio/mp3', 'audio/wma']
   // "etc":
 };
